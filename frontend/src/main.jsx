95d7324c9d160e89801c8d6872e5ef3306741c4f
kriaa9 <11596@holbertonstudents.com>
2026-01-25 01:50:59 +0100

    feat: Implement initial e-commerce backend and frontend with Docker Compose integration.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import {DevSupport} from "@react-buddy/ide-toolbox";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </React.StrictMode>
 );
